5b19ee23a4571653e27375ff9a58b8bad7b6fef6
Nguyễn Thu Thảo Châu <63655372+nguyenthuthaochau2201@users.noreply.github.com>
2025-06-23 13:59:45 +0700

    Update driver.py

--- src/flockwave/server/ext/mavlink/driver.py
+++ src/flockwave/server/ext/mavlink/driver.py
@@ -2156,12 +2156,12 @@ class MAVLinkUAV(UAVBase):
         # TODO(ntamas): this is not entirely accurate due to the back-and-forth
         # conversion happening between floats and ints; sometimes the 7th
         # decimal digit is off by one.
-        encoded_lat = int(coordinate_system.origin.lat * 1e7)
-        encoded_lon = int(coordinate_system.origin.lon * 1e7)
+        encoded_lat = int(coordinate_system.origin.lat)
+        encoded_lon = int(coordinate_system.origin.lon )
         encoded_amsl = (
-            int(altitude_reference * 1e3)
+            int(altitude_reference)
             if altitude_reference is not None
-            else -32768000
+            else -32768
         )
 
         # Try configuring with a single USER_2 command first, falling back to
